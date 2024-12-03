a7f60c747ec8f767230b3ab8119f67427d60497d
Martynas Deveikis <martynasdev@gmail.com>
2024-12-03 14:19:52 +0200

    fix: use previous image when reinstalling server
    When reinstalling server, if no new image option
    has been provided, use the previous one.

--- plugins/modules/server.py
+++ plugins/modules/server.py
@@ -360,6 +360,8 @@ class ServerModule(standard_module.StandardModule):
                 reinstall_req[k] = params[k]
 
         if reinstall_req:
+            if reinstall_req.get("image", None) is None:
+                reinstall_req["image"] = resource["image"]
             reinstall_req["password"] = generate_password(16)
             reinstall_req["type"] = "reinstall"
             req["reinstall"] = reinstall_req
